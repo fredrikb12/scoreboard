966ccc1f63b34b4b39986e9b81b29c62255a3a73
fredrikb12 <fredrik.backman12@gmail.com>
2022-09-09 15:48:59 +0200

    Refactor playerHasWon to be more general

--- src/utils/points/playerHasWon.ts
+++ src/utils/points/playerHasWon.ts
@@ -1,5 +1,9 @@
-export function playerHasWon(scoringPlayerPoints: number, p2Points: number) {
-  if (scoringPlayerPoints > 3 && scoringPlayerPoints - p2Points >= 2) {
+export function playerHasWon(p1Points: number, p2Points: number) {
+  let leader, p2;
+  leader = p1Points > p2Points ? p1Points : p2Points;
+  p2 = p1Points > p2Points ? p2Points : p1Points;
+
+  if (leader > 3 && leader - p2 >= 2) {
     return true;
   } else {
     return false;
